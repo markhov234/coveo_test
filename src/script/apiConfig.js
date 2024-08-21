1919dcbb244418cd1d5422cb204a3331914d4b02
Markhov <hovington234@gmail.com>
2024-08-20 21:17:07 -0400

    added cors

--- src/script/apiConfig.js
+++ src/script/apiConfig.js
@@ -3,19 +3,16 @@
 // Determine if the environment is local or production
 const isLocal = window.location.hostname === "localhost";
 
-console.log("isLocal", isLocal);
-
 // URLs for API requests
 const speakersUrl = isLocal
   ? process.env.APP_SPEAKERS_URL_LOCAL
-  : process.env.APP_SPEAKERS_URL;
+  : "/.netlify/functions/speakers"; // Use Netlify function for production
 
-console.log("speakersUrl", speakersUrl);
 const eventsUrl = isLocal
   ? process.env.APP_EVENTS_URL_LOCAL
-  : process.env.APP_EVENTS_URL;
+  : "/.netlify/functions/events"; // Use Netlify function for production
 
-// Do not expose the API key directly in client-side code
+// API key should not be exposed directly in client-side code
 const apiKey = isLocal ? process.env.APP_API_KEY_LOCAL : "";
 
 export { speakersUrl, eventsUrl, apiKey };
