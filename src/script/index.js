cb3562d798cf080bfa74e6929b29cc9f42d68652
Markhov <hovington234@gmail.com>
2024-08-20 22:32:18 -0400

    forgot comments

--- src/script/index.js
+++ src/script/index.js
@@ -5,8 +5,8 @@ import { handleFormSubmit } from "./formHandler.js"; // Import form handler
 import "../style/sass/style.scss";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  // await loadSpeakers();
-  // await loadMainEvent();
+  await loadSpeakers();
+  await loadMainEvent();
 
   // Attach the form handler
   const form = document.querySelector("#registration-form");
