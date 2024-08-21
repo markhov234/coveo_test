36aed60a4535d8ed5f9abe8f432854be026cdd64
Markhov <hovington234@gmail.com>
2024-08-20 20:58:37 -0400

    Localhost working

--- src/script/apiConfig.js
+++ src/script/apiConfig.js
@@ -1,8 +1,21 @@
 // src/apiConfig.js
 
+// Determine if the environment is local or production
 const isLocal = window.location.hostname === "localhost";
 
-const speakersUrl = isLocal ? "/api/speakers" : "/.netlify/functions/speakers";
-const eventsUrl = isLocal ? "/api/events" : "/.netlify/functions/events";
+console.log("isLocal", isLocal);
 
-export { speakersUrl, eventsUrl };
+// URLs for API requests
+const speakersUrl = isLocal
+  ? process.env.APP_SPEAKERS_URL_LOCAL
+  : process.env.APP_SPEAKERS_URL;
+
+console.log("speakersUrl", speakersUrl);
+const eventsUrl = isLocal
+  ? process.env.APP_EVENTS_URL_LOCAL
+  : process.env.APP_EVENTS_URL;
+
+// Do not expose the API key directly in client-side code
+const apiKey = process.env.APP_API_KEY;
+
+export { speakersUrl, eventsUrl, apiKey };
